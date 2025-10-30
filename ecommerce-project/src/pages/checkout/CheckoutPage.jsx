41b53c335aaa1bd249d491c61df1e9aecde05b81
Sonu <sonuroy2029@gmail.com>
2025-10-30 15:23:48 +0530

    refactor: separate fetchPaymentSummary

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -12,14 +12,20 @@ export default function CheckoutPage({ cart, loadCart }) {
 
   useEffect(() => {
     const fetchCheckoutData = async () => {
-      let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
+      const response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
       setDeliveryOptions(response.data);
-  
-      response = await axios.get('/api/payment-summary');
-      setPaymentSummary(response.data);
     };
 
     fetchCheckoutData();
+  }, []);
+
+  useEffect(() => {
+    const fetchPaymentSummary = async () => {
+      const response = await axios.get('/api/payment-summary');
+      setPaymentSummary(response.data);
+    }
+
+    fetchPaymentSummary();
   }, [cart]);
 
   return (
