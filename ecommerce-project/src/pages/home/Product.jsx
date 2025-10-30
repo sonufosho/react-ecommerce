a72b1eb596125b4415ee9185707f19aa8c034825
Sonu <sonuroy2029@gmail.com>
2025-10-30 16:37:14 +0530

    feat: render added message

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -4,14 +4,20 @@ import { formatMoney } from "../../utils/money";
 
 export function Product({ product, loadCart }) {
   const [quantity, setQuantity] = useState(1);
+  const [showAddedMessage, setShowAddedMessage] = useState(false);
 
   const addToCart = async () => {
     await axios.post('/api/cart-items', {
       productId: product.id,
       quantity: quantity
     });
-
     loadCart();
+
+    setShowAddedMessage(true);
+
+    setTimeout(() => {
+      setShowAddedMessage(false);
+    }, 2000);
   }
 
   const selectQuantity = (event) => {
@@ -59,7 +65,9 @@ export function Product({ product, loadCart }) {
 
       <div className="product-spacer"></div>
 
-      <div className="added-to-cart">
+      <div className="added-to-cart" style={{
+        opacity: `${showAddedMessage ? 1 : 0}`
+      }}>
         <img src="images/icons/checkmark.png" />
         Added
       </div>
