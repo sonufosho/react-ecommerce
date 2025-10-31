ccaba1fd31ba5fa9273dbfa506aba11efc0bb0dc
Sonu <sonuroy2029@gmail.com>
2025-10-31 16:24:00 +0530

    feat: add keyboard events to quantity input

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -28,6 +28,18 @@ export function CartItemDetails({ cartItem, loadCart }) {
     setQuantity(event.target.value);
   };
 
+  const handleQuantityKeyDown = (event) => {
+    const keyPressed = event.key;
+
+    if (keyPressed === 'Enter') {
+      updateQuantity();
+
+    } else if (keyPressed === 'Escape') {
+      setQuantity(cartItem.quantity);
+      setIsUpdatingQuantity(false);
+    }
+  };
+
   return (
     <>
       <img className="product-image"
@@ -44,7 +56,8 @@ export function CartItemDetails({ cartItem, loadCart }) {
           <span>
             Quantity: {isUpdatingQuantity
               ? <input type="text" className="quantity-textbox"
-                  value={quantity} onChange={updateQuantityInput}/>
+                  value={quantity} onChange={updateQuantityInput}
+                  onKeyDown={handleQuantityKeyDown} />
               : <span className="quantity-label">{cartItem.quantity}</span>
             }
           </span>
